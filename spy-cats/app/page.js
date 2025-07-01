0b0e121c69793327276eeade5e750a9d1a0af1d7
Shushunya <mremarka314@gmail.com>
2025-07-01 16:04:49 +0100

    adjustments

--- spy-cats/app/page.js
+++ spy-cats/app/page.js
@@ -1,29 +1,22 @@
 "use client";
 
-import 'primereact/resources/themes/saga-blue/theme.css'; // Theme (choose your preferred theme)
-import 'primereact/resources/primereact.min.css';         // Core CSS
-import 'primeicons/primeicons.css';                      // Icons
-
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 import React, { useState, useRef } from 'react';
+
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+
 import { Button } from 'primereact/button';
+
 import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
 import { Toast } from 'primereact/toast';
 import { Dialog } from 'primereact/dialog';
+
 import { InputNumber } from 'primereact/inputnumber';
 import { InputText } from "primereact/inputtext";
-
-
-
-// interface Cat {
-//   id: number;
-//   name: string;
-//   yearsOfExperience: number;
-//   salary: number;
-//   breed: string;
-// }
 
 
 export default function Management() {
@@ -37,12 +30,12 @@ export default function Management() {
   const [salary, setSalary] = useState(0);
 
   const columns = [
-        {field: 'id', header: 'ID'},
-        {field: 'name', header: 'Name'},
-        {field: 'years_of_exp', header: 'Experience'},
-        {field: 'salary', header: 'Salary'},
-        {field: 'breed', header: 'Breed'}
-    ];
+    { field: 'id', header: 'ID' },
+    { field: 'name', header: 'Name' },
+    { field: 'years_of_exp', header: 'Experience' },
+    { field: 'salary', header: 'Salary' },
+    { field: 'breed', header: 'Breed' }
+  ];
 
   const toast = useRef(null);
 
@@ -56,7 +49,7 @@ export default function Management() {
       });
       if (!response.ok) throw new Error('Failed to delete cat');
       const data = await response.json();
-      console.log('Deleted cat:', data);
+      console.log(data);
       return data;
     } catch (error) {
       console.error('Error deleting cat:', error);
@@ -80,18 +73,18 @@ export default function Management() {
       },
       body: JSON.stringify(newCat),
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log('New cat added:', data);
-      setCats([...cats, data]);
-      toast.current.show({ severity: 'success', summary: 'Success', detail: `${data.name} has been added`, life: 3000 });
-    })
-    .catch(error => {
-      console.error('Error adding cat:', error);
-      toast.current.show({ severity: 'error', summary: 'Error', detail: 'Failed to add cat', life: 3000 });
-    });
+      .then(response => response.json())
+      .then(data => {
+        console.log('New cat added:', data);
+        setCats([...cats, data]);
+        toast.current.show({ severity: 'success', summary: 'Success', detail: `${data.name} has been added`, life: 3000 });
+      })
+      .catch(error => {
+        console.error('Error adding cat:', error);
+        toast.current.show({ severity: 'error', summary: 'Error', detail: 'Failed to add cat', life: 3000 });
+      });
 
-    setVisible(false);
+    setVisibleCreate(false);
   };
 
   const confirmDelete = () => {
@@ -125,23 +118,24 @@ export default function Management() {
     };
     console.log(updatedCat)
     // Logic to update the cat's salary in the backend
-    fetch(`http://localhost:8000/cats/${cat.id}?new_salary=${salary}`, {
+    fetch(`http://localhost:8000/cats/${cat.id}`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(updatedCat),
+
+      body: salary,
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Cat updated:', data);
-      setCats(cats.map(c => c.id === cat.id ? data : c));
-      setVisibleUpdate(false);
-      toast.current.show({ severity: 'success', summary: 'Success', detail: `${data.name}'s salary has been updated`, life: 3000 });
-    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Cat updated:', data);
+        setCats(cats.map(c => c.id === cat.id ? data : c));
+        setVisibleUpdate(false);
+        toast.current.show({ severity: 'success', summary: 'Success', detail: `${data.name}'s salary has been updated`, life: 3000 });
+      })
 
   };
-  
+
   React.useEffect(() => {
     async function fetchCats() {
       try {
@@ -168,13 +162,11 @@ export default function Management() {
     setSalary(e.value.salary);
   };
 
-  
 
-  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-8">
       <h1 className="text-2xl font-bold">Management Page</h1>
-      <ul>
+      <ul className="text-xl">
         <li className="mb-4">This page is for managing spy cats.</li>
         <li className="mb-4">You can perform the following actions:</li>
         <ul className="list-disc pl-6">
@@ -185,60 +177,61 @@ export default function Management() {
         </ul>
       </ul>
 
-      <div>
-        <h1>Cats list table</h1>
-        <DataTable 
-          value={cats} 
-          showGridlines 
-          selectionMode="single" 
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold">Cats list table</h1>
+        <DataTable
+          value={cats}
+          showGridlines
+          selectionMode="single"
           selection={selectedCat}
           onSelectionChange={selectCatRow}
           tableStyle={{ minWidth: '50rem' }}>
-          
+
           {columns.map((col) => (
             <Column key={col.field} field={col.field} header={col.header} />
           ))}
         </DataTable>
 
       </div>
-          <Toast ref={toast} />
-            <ConfirmDialog />
+      <Toast ref={toast} />
+      <ConfirmDialog />
       <div className="card flex flex-wrap justify-content-center gap-3">
-            <Button onClick={setVisibleCreate} label="Add" icon="pi pi-check" />
-            <Button onClick={setVisibleUpdate} label="Update" icon="pi pi-check" />
-            <Button onClick={confirmDelete} label="Delete" icon="pi pi-times" iconPos="right"  severity="danger"/>
-            
-            <Dialog header="Add a cat" visible={visibleCreate} style={{ width: '50vw' }} onHide={() => {if (!visibleCreate) return; setVisibleCreate(false); }}>
-                <p className="m-0">
-                    Please fill in the details of the new cat you want to add.
-                    Name and Years of Experience are required fields.
-                </p>
-                <div className="card flex flex-wrap gap-3 p-fluid">
-                  <div className="flex-auto">
-                      <label htmlFor="name" className="font-bold block mb-2">Name</label>
-                      <InputText id="name" value={name} onChange={(e) => setName(e.target.value)} />
-                    </div>
-                  <div className="flex-auto">
-                      <label htmlFor="yearsOfExperience" className="font-bold block mb-2">Years of Experience</label>
-                      <InputNumber inputId="yearsOfExperience" value={yearsOfExperience} onValueChange={(e) => setYearsOfExperience(e.value)} />
-                  </div>
-                  <Button onClick={proceedCreate} label="Confirm" icon="pi pi-check" />
-                </div>
-            </Dialog>
+        <Button onClick={setVisibleCreate} label="Add" icon="pi pi-check" />
+        <Button onClick={setVisibleUpdate} label="Update" icon="pi pi-check" />
+        <Button onClick={confirmDelete} label="Delete" icon="pi pi-times" iconPos="right" severity="danger" />
 
-            <Dialog header="Update Cat" visible={visibleUpdate} style={{ width: '50vw' }} onHide={() => {if (!visibleUpdate) return; setVisibleUpdate(false); }}>
-              <p className="m-0">
-                    Please enter the new salary for the selected cat.
-                </p>
-                <div className="card flex flex-wrap gap-3 p-fluid">
-                  <div className="flex-auto">
-                      <label htmlFor="salary" className="font-bold block mb-2">Salary</label>
-                      <InputNumber inputId="salary" value={salary} onValueChange={(e) => setSalary(e.value)} />
-                  </div>
-                  <Button onClick={proceedUpdate} label="Confirm" icon="pi pi-check" />
-                </div>
-            </Dialog>
-        </div>
+        <Dialog header="Add a cat" visible={visibleCreate} style={{ width: '50vw' }} onHide={() => { if (!visibleCreate) return; setVisibleCreate(false); }}>
+          <p className="m-0">
+            Please fill in the details of the new cat you want to add.
+            Name and Years of Experience are required fields.
+          </p>
+          <div className="card flex flex-wrap gap-3 p-fluid">
+            <div className="flex-auto">
+              <label htmlFor="name" className="font-bold block mb-2">Name</label>
+              <InputText id="name" value={name} onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div className="flex-auto">
+              <label htmlFor="yearsOfExperience" className="font-bold block mb-2">Years of Experience</label>
+              <InputNumber inputId="yearsOfExperience" value={yearsOfExperience} onValueChange={(e) => setYearsOfExperience(e.value)} />
+            </div>
+            <Button onClick={proceedCreate} label="Confirm" icon="pi pi-check" />
+          </div>
+        </Dialog>
+
+
+        <Dialog header="Update Cat" visible={visibleUpdate} style={{ width: '50vw' }} onHide={() => { if (!visibleUpdate) return; setVisibleUpdate(false); }}>
+          <p className="m-0">
+            Please enter the new salary for the selected cat.
+          </p>
+          <div className="card flex flex-wrap gap-3 p-fluid">
+            <div className="flex-auto">
+              <label htmlFor="salary" className="font-bold block mb-2">Salary</label>
+              <InputNumber inputId="salary" value={salary} onValueChange={(e) => setSalary(e.value)} />
+            </div>
+            <Button onClick={proceedUpdate} label="Confirm" icon="pi pi-check" />
+          </div>
+        </Dialog>
+      </div>
     </div>
   );
 }
